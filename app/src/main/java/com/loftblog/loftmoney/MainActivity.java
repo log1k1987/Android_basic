fc47fd71b167fab1ee8237e2e03631301fba7c1a
log1k <log1k@mail.ru>
2019-06-23 20:14:25 +0300

    add icon

--- app/src/main/java/com/loftblog/loftmoney/MainActivity.java
+++ app/src/main/java/com/loftblog/loftmoney/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView merge = findViewById(R.id.merge);
+        Button merge = findViewById(R.id.merge);
         merge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
